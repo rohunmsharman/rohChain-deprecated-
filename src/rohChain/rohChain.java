fef015fead0857e6c55409fe029193f1d447741a
rohunmsharman <66086705+rohunmsharman@users.noreply.github.com>
2020-08-19 16:07:31 -0400

    quick test for the wallets and sigs, located in rohChain.main

--- src/rohChain/rohChain.java
+++ src/rohChain/rohChain.java
@@ -1,18 +1,42 @@
-package rohChain; //written by Rohun Sharman (started 7/9/2020)
+package rohChain; //created by rohun sharman (with help from the internet)
 
 import java.util.ArrayList;
 //import com.mongodb.client;
 import com.google.gson.GsonBuilder;
-
+import java.util.ArrayList;
+import java.util.Base64;
+import java.security.Security;
 
 public class rohChain {
 
     public static ArrayList<block> rChain = new ArrayList<block>();
-
     public static int difficulty = 5;
+    public static wallet walletA;
+    public static wallet walletB;
 
     public static void main(String[] args){
-        genesisBlock();
+        //bouncy castle security provider
+        Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
+
+        //create test wallets
+        walletA = new wallet();
+        walletB = new wallet();
+
+        // test pub/priv keys
+        System.out.println("priv and pub keys:");
+        System.out.println(StringUtil.getStringFromKey(walletA.privateKey));
+        System.out.println(StringUtil.getStringFromKey(walletA.publicKey));
+
+        //test txn walletA to walletB
+        txn txn1 = new txn(walletA.publicKey, walletB.publicKey, 5, null);
+        txn1.genSig(walletA.privateKey);
+
+        //verify signing works from pub key
+        System.out.println("is sig valid? ");
+        System.out.println(txn1.verifySignature());
+
+
+        /*genesisBlock();
         addMessage.addMessage();
         System.out.println("chain valid? " + isChainValid());
 
@@ -20,6 +44,7 @@ public class rohChain {
         System.out.println("current blockchain");
         System.out.println(blockchainJson);
 
+         */
     }
 
     //adds genesis rohChain.rohChain.block and mines it
